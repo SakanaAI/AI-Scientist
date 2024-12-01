0463e512d2d8ac5eed647789fd76bc1a0431e429
Nhat <53397242+nhatvu192@users.noreply.github.com>
2024-12-01 16:29:31 +0000

    Change default value for eval to always evaluate on test dataset

--- templates/gaussian_splatting/arguments/__init__.py
+++ templates/gaussian_splatting/arguments/__init__.py
@@ -55,7 +55,7 @@ class ModelParams(ParamGroup):
         self._white_background = False
         self.train_test_exp = False
         self.data_device = "cuda"
-        self.eval = False
+        self.eval = True
         super().__init__(parser, "Loading Parameters", sentinel)
 
     def extract(self, args):
